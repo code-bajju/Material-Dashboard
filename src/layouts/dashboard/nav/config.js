a737383c537f4965fb7711641422405ebe8b32ad
code-bajju <11212766@mmec.mmdu.org>
2023-08-10 10:47:16 +0530

    Upated config

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -41,11 +41,6 @@ const navConfig = [
     path: '/login',
     icon: icon('ic_lock'),
   },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: icon('ic_disabled'),
-  },
 ];
 
 export default navConfig;
