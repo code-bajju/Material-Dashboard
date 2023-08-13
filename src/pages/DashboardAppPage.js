d8b9c3454decf653373d46bcf556bae25b62e338
code-bajju <11212766@mmec.mmdu.org>
2023-08-13 21:57:40 +0530

    Updates

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet-async';
 import { faker } from '@faker-js/faker';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography } from '@mui/material';
+import { Grid, Container, Typography , Card , CardContent , Rating } from '@mui/material';
 // components
 import Iconify from '../components/iconify';
 // sections
@@ -22,6 +22,7 @@ import {
 
 export default function DashboardAppPage() {
   const theme = useTheme();
+  
 
   return (
     <>
@@ -155,21 +156,26 @@ export default function DashboardAppPage() {
           </Grid>
 
           <Grid item xs={12} md={6} lg={4}>
-            <AppOrderTimeline
-              title="Top Rated Resume"
-              list={[...Array(5)].map((_, index) => ({
-                id: faker.datatype.uuid(),
-                title: [
-                  '1983, orders, $4220',
-                  '12 Invoices have been paid',
-                  'Order #37745 from September',
-                  'New order placed #XF-2356',
-                  'New order placed #XF-2346',
-                ][index],
-                type: `order${index + 1}`,
-                time: faker.date.past(),
-              }))}
-            />
+          <Card variant="outlined">
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          My Resume
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Here's my top-rated resume:
+        </Typography>
+        <Typography variant="body2">
+          - Experience: [Your experience details]
+        </Typography>
+        <Typography variant="body2">
+          - Skills: [Your skills details]
+        </Typography>
+        <Typography variant="body2">
+          - Education: [Your education details]
+        </Typography>
+        <Rating name="top-rated" value={5} readOnly />
+      </CardContent>
+    </Card>
           </Grid>
 
           <Grid item xs={12} md={6} lg={4}>
