a948c0fd658cdb4b5d1c608f196ba7263a74be70
code-bajju <11212766@mmec.mmdu.org>
2023-08-14 09:35:45 +0530

    Dashboard Completed

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -17,6 +17,7 @@ import {
   AppCurrentSubject,
   AppConversionRates,
 } from '../sections/@dashboard/app';
+import TopRated from './TopRated';
 
 // ----------------------------------------------------------------------
 
@@ -109,103 +110,6 @@ export default function DashboardAppPage() {
               ]}
             />
           </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppConversionRates
-              title="Conversion Rates"
-              subheader="(+43%) than last year"
-              chartData={[
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentSubject
-              title="Current Subject"
-              chartLabels={['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math']}
-              chartData={[
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-              ]}
-              chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppNewsUpdate
-              title="News Update"
-              list={[...Array(5)].map((_, index) => ({
-                id: faker.datatype.uuid(),
-                title: faker.name.jobTitle(),
-                description: faker.name.jobTitle(),
-                image: `/assets/images/covers/cover_${index + 1}.jpg`,
-                postedAt: faker.date.recent(),
-              }))}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-          <Card variant="outlined">
-      <CardContent>
-        <Typography variant="h5" gutterBottom>
-          My Resume
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          Here's my top-rated resume:
-        </Typography>
-        <Typography variant="body2">
-          - Experience: [Your experience details]
-        </Typography>
-        <Typography variant="body2">
-          - Skills: [Your skills details]
-        </Typography>
-        <Typography variant="body2">
-          - Education: [Your education details]
-        </Typography>
-        <Rating name="top-rated" value={5} readOnly />
-      </CardContent>
-    </Card>
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTrafficBySite
-              title="Traffic by Site"
-              list={[
-                {
-                  name: 'FaceBook',
-                  value: 323234,
-                  icon: <Iconify icon={'eva:facebook-fill'} color="#1877F2" width={32} />,
-                },
-                {
-                  name: 'Google',
-                  value: 341212,
-                  icon: <Iconify icon={'eva:google-fill'} color="#DF3E30" width={32} />,
-                },
-                {
-                  name: 'Linkedin',
-                  value: 411213,
-                  icon: <Iconify icon={'eva:linkedin-fill'} color="#006097" width={32} />,
-                },
-                {
-                  name: 'Twitter',
-                  value: 443232,
-                  icon: <Iconify icon={'eva:twitter-fill'} color="#1C9CEA" width={32} />,
-                },
-              ]}
-            />
-          </Grid>
-
           <Grid item xs={12} md={6} lg={8}>
             <AppTasks
               title="Tasks"
@@ -218,6 +122,21 @@ export default function DashboardAppPage() {
               ]}
             />
           </Grid>
+        
+
+          <Grid item xs={12} md={6} lg={4}>
+          <Card variant="outlined">
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          My Resume
+        </Typography>
+      <TopRated/>
+      </CardContent>
+    </Card>
+          </Grid>
+
+
+         
         </Grid>
       </Container>
     </>
