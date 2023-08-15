c06482c511e2022bc813dddb605f08141c12db1d
code-bajju <11212766@mmec.mmdu.org>
2023-08-15 18:50:30 +0530

    Index of Registration updated

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,20 +1,17 @@
 import { Helmet } from 'react-helmet-async';
-// @mui
 import { styled } from '@mui/material/styles';
 import { Link, Container, Typography, Divider, Stack, Button } from '@mui/material';
-// hooks
 import useResponsive from '../hooks/useResponsive';
-// components
 import Logo from '../components/logo';
 import Iconify from '../components/iconify';
-// sections
 import { LoginForm } from '../sections/auth/login';
-
-// ----------------------------------------------------------------------
 
 const StyledRoot = styled('div')(({ theme }) => ({
   [theme.breakpoints.up('md')]: {
     display: 'flex',
+    backgroundImage: `url('/assets/illustrations/bg.jpg')`,
+    backgroundSize: 'cover',
+    backgroundColor: '#96144C',
   },
 }));
 
@@ -25,7 +22,7 @@ const StyledSection = styled('div')(({ theme }) => ({
   flexDirection: 'column',
   justifyContent: 'center',
   boxShadow: theme.customShadows.card,
-  backgroundColor: theme.palette.background.default,
+  backgroundColor: 'rgba(255, 255, 255, 0.8)',
 }));
 
 const StyledContent = styled('div')(({ theme }) => ({
@@ -38,8 +35,6 @@ const StyledContent = styled('div')(({ theme }) => ({
   padding: theme.spacing(12, 0),
 }));
 
-// ----------------------------------------------------------------------
-
 export default function LoginPage() {
   const mdUp = useResponsive('up', 'md');
 
@@ -50,51 +45,23 @@ export default function LoginPage() {
       </Helmet>
 
       <StyledRoot>
-        <Logo
-          sx={{
-            position: 'fixed',
-            top: { xs: 16, sm: 24, md: 40 },
-            left: { xs: 16, sm: 24, md: 40 },
-          }}
-        />
-
-        {mdUp && (
-          <StyledSection>
-            <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-              Hi, Welcome Back
-            </Typography>
-            <img src="/assets/illustrations/illustration_login.png" alt="login" />
-          </StyledSection>
-        )}
+       
 
         <Container maxWidth="sm">
           <StyledContent>
-            <Typography variant="h4" gutterBottom>
-              Sign in to Minimal
+            <Typography style={{color:'white',textAlign:'center'}} variant="h4" gutterBottom>
+              Login
             </Typography>
 
-            <Typography variant="body2" sx={{ mb: 5 }}>
+            <Typography style={{color:'white',textAlign:'center'}} variant="body2" sx={{ mb: 5 }}>
               Don’t have an account? {''}
-              <Link variant="subtitle2">Get started</Link>
+              <Link style={{color:'#96144C'}} variant="subtitle2">Get started</Link>
             </Typography>
 
-            <Stack direction="row" spacing={2}>
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
-              </Button>
-            </Stack>
-
-            <Divider sx={{ my: 3 }}>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                OR
+            
+            <Divider  style={{color:'white'}} sx={{ my: 3 }}>
+              <Typography style={{color:'white'}} variant="body2" sx={{ color: 'text.secondary' }}>
+             Login to Your Account
               </Typography>
             </Divider>
 
